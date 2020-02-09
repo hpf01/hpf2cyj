6368ab7ecb4e00a4b171eeed2165978bb230c763
hpf01 <38858123+hpf01@users.noreply.github.com>
2020-02-09 11:45:51 +0800

    Update move.js

--- move.js
+++ move.js
@@ -1,52 +1,75 @@
-;
-//通过class获取元素
-function getClass(cls){
-    var ret = [];
-    var els = document.getElementsByTagName("*");
-    for (var i = 0; i < els.length; i++){
-        //判断els[i]中是否存在cls这个className;.indexOf("cls")判断cls存在的下标，如果下标>=0则存在;
-        if(els[i].className === cls || els[i].className.indexOf("cls")>=0 || els[i].className.indexOf(" cls")>=0 || els[i].className.indexOf(" cls ")>0){
-            ret.push(els[i]);
+ function ajax(method, url, callback, data, flag) {
+    var xhr = null;
+    if(window.XMLHttpRequest) {
+        xhr =  new XMLHttpRequest();
+    }else {
+        xhr = new ActiveXObject('Microsoft.XMLHttp')
+    }
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4) {
+            if(xhr.status == 200) {
+                callback(xhr.responseText);
+            }else {
+                console.log('error');
+            }
         }
     }
-    return ret;
+    method = method.toUpperCase();
+    if(method == 'GET') {
+        var date = new Date(),
+            timer = date.getTime();
+        xhr.open(method, url + '?' + data + '&timer=' + timer, flag);
+        xhr.send();
+    }else if(method == 'POST') {
+        xhr.open(method, url, flag);
+        xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+        xhr.send(data);
+    }
 }
-function getStyle(obj,attr){//解决JS兼容问题获取正确的属性值
-	return obj.currentStyle?obj.currentStyle[attr]:getComputedStyle(obj,false)[attr];
+
+  
+  
+function init () {
+    bindEvent();
 }
-function startMove(obj,json,fun){
-	clearInterval(obj.timer);
-	obj.timer = setInterval(function(){
-		var isStop = true;
-		for(var attr in json){
-			var iCur = 0;
-			//判断运动的是不是透明度值
-			if(attr=="opacity"){
-				iCur = parseInt(parseFloat(getStyle(obj,attr))*100);
-			}else{
-				iCur = parseInt(getStyle(obj,attr));
-			}
-			var ispeed = (json[attr]-iCur)/8;
-			//运动速度如果大于0则向下取整，如果小于0想上取整；
-			ispeed = ispeed>0?Math.ceil(ispeed):Math.floor(ispeed);
-			//判断所有运动是否全部完成
-			if(iCur!=json[attr]){
-				isStop = false;
-			}
-			//运动开始
-			if(attr=="opacity"){
-				obj.style.filter = "alpha:(opacity:"+(json[attr]+ispeed)+")";
-				obj.style.opacity = (json[attr]+ispeed)/100;
-			}else{
-				obj.style[attr] = iCur+ispeed+"px";
-			}
-		}
-		//判断是否全部完成
-		if(isStop){
-			clearInterval(obj.timer);
-			if(fun){
-				fun();
-			}
-		}
-	},30);
+function bindEvent() {
+    $('#inp').keyup(function (e) {
+        if (e.keyCode == 13) {
+            $('#submit').trigger('click');
+        }
+    });
+    $('#submit').click(function (e) {
+        var val = $('#inp').val();
+        if (val) {
+            addDom('mine', val);
+            getData(val);
+            $('#inp').val('');
+        }
+    })
 }
+
+// 文字添加到页面中去
+function addDom(who, val) {
+    $(' <div class="' + who + '">\
+    <div class="avitor"></div>\
+    <div class="text">' + val + '</div>\
+</div>').appendTo($('.content-box'));
+    var scrollHeight = $('.content-box')[0].scrollHeight ;
+    var innerHeight = $('.content-box').innerHeight();
+    $('.content-box').scrollTop(scrollHeight - innerHeight);
+}
+
+ // var paras = "key= 72a5507158ff4ca5aa05043d4c24ce55&info="+sayContent;
+
+function getData(val) {
+        var paras = "key=你的key&info="+val;
+         ajax('POST',"http://www.tuling123.com/openapi/api",success, paras, true)
+          function success(data){
+            // console.log(data)
+          da=eval("("+data+")")
+          addDom('robot', da.text);
+          }
+    
+}
+init()
+
